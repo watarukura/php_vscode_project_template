b36d50e83b5f79e280caa3c21f3bd6b7ec5a1483
wataru-kurashima <wataru-kurashima@hands-lab.com>
2020-10-31 17:27:52 +0900

    feat: add unit-test and integration-test

--- config/routes.php
+++ config/routes.php
@@ -4,8 +4,6 @@ use Slim\App;
 
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class)->setName('home');
-};
-
-return function (App $app) {
-    $app->post('/users', \App\Action\UserCreateAction::class);
+    $app->get('/users/{id}', \App\Action\UserReadAction::class)->setName('users-get');
+    $app->post('/users', \App\Action\UserCreateAction::class)->setName('users-post');
 };
