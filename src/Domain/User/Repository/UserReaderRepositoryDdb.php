40f758c510a8f21f20790b0d9367ef2ab0a9807a
wataru-kurashima <wataru-kurashima@hands-lab.com>
2021-02-12 07:54:53 +0900

    feat(ddb): use PartiQL with executeStatement()

--- src/Domain/User/Repository/UserReaderRepositoryDdb.php
+++ src/Domain/User/Repository/UserReaderRepositoryDdb.php
@@ -40,29 +40,52 @@ class UserReaderRepositoryDdb
      */
     public function getUserById(int $userId): UserReaderData
     {
+//        try {
+//            $marshaler = new Marshaler();
+//            $key = $marshaler->marshalJson((string)json_encode([
+//                'id' => $userId
+//            ]));
+//            $row = $this->client->getItem([
+//                'TableName'      => 'users',
+//                'Key'            => $key,
+//            ]);
+//            if (!$row->get('Item')) {
+//                throw new DomainException(sprintf('User not found: %s', $userId));
+//            }
+//        } catch (Exception $exception) {
+//            throw $exception;
+//        }
         try {
-            $marshaler = new Marshaler();
-            $key = $marshaler->marshalJson((string)json_encode([
-                'id' => $userId
-            ]));
-            $row = $this->client->getItem([
-                'TableName'      => 'users',
-                'Key'            => $key,
+            $result = $this->client->executeStatement([
+//                'ConsistentRead' => true,
+                'Parameters' => [
+                    [
+                        'N' => $userId
+                    ]
+                ],
+                'Statement' => 'SELECT * FROM users WHERE id = ?'
             ]);
-            if (!$row->get('Item')) {
+            if (!$result->get('Items')) {
                 throw new DomainException(sprintf('User not found: %s', $userId));
             }
+            var_dump($result->get('Items')[0]);
         } catch (Exception $exception) {
             throw $exception;
         }
 
+
         // Map array to data object
         $user = new UserReaderData();
-        $user->id = (int)$row['Item']['id']['N'];
-        $user->username = (string)$row['Item']['username']['S'];
-        $user->first_name = (string)$row['Item']['first_name']['S'];
-        $user->last_name = (string)$row['Item']['last_name']['S'];
-        $user->email = (string)$row['Item']['email']['S'];
+//        $user->id = (int)$row['Item']['id']['N'];
+//        $user->username = (string)$row['Item']['username']['S'];
+//        $user->first_name = (string)$row['Item']['first_name']['S'];
+//        $user->last_name = (string)$row['Item']['last_name']['S'];
+//        $user->email = (string)$row['Item']['email']['S'];
+//        $user->id = (int)$result['Items'][0]['id']['N'];
+        $user->username = (string)$result['Items'][0]['username']['S'];
+        $user->first_name = (string)$result['Items'][0]['first_name']['S'];
+        $user->last_name = (string)$result['Items'][0]['last_name']['S'];
+        $user->email = (string)$result['Items'][0]['email']['S'];
 
         return $user;
     }
